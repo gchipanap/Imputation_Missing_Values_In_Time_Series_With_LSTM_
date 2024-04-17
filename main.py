2912db11f0715f9f8a25db545cffc4b52b947e3b
Gabriela <64268942+gchipanap@users.noreply.github.com>
2024-04-17 11:08:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,153 +10,145 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import regularizers
 from custom_loss import  Ltotal
 
+# Define flags
+class FLAGS:
+    num_layers = 1
+    hidden_size = 128
+    embedding_size = 3
+    missing_flag = -1.0
 
-# Función para generar el Residual Sum Vector (RSV)
-def generate_RSV(model, X):
-    hiddens = model.predict(X)
-    max_length = max(item.shape[0] for item in hiddens)
-    
-    rsv = []
-    for hidden in hiddens:
-        pad_width = ((0, max_length - hidden.shape[0]), (0, 0))
-        padded_hidden = np.pad(hidden, pad_width, mode='constant')
-        rsv.append(padded_hidden)
-    
-    return np.concatenate(rsv, axis=1)
+class LSTM_RSV(tf.keras.Model):    
+    def __init__(self, is_training, config, FLAGS):
+        super(LSTM_RSV, self).__init__()
+        self.batch_size = config.batch_size
+        self.num_steps = config.num_steps 
+        size = config.hidden_size  
+        embedding_size = config.embedding_size 
+
+        self.input_data = tf.keras.Input(shape=(self.num_steps, embedding_size), batch_size=self.batch_size)  # input
+        self.targets = tf.keras.Input(shape=(self.num_steps, embedding_size), batch_size=self.batch_size)  # output
+        lstm_cell = tf.keras.layers.LSTMCell(size, forget_bias=0.0)
+        cell = tf.keras.layers.StackedRNNCells([lstm_cell] * config.num_layers)
+        self.initial_state = cell.get_initial_state(batch_size=self.batch_size, dtype=tf.float32)
+
+        state = self.initial_state 
+        self.F=[]
+
+        for time_step in range(self.num_steps):
+            if time_step > 0:
+                tf.keras.backend.set_learning_phase(1)
+            if time_step == 0:
+                (cell_output, state) = cell(self.input_data[:, time_step, :], initial_state=state)
+                self.F.append(cell_output)
+            else:
+                comparison = tf.equal(self.input_data[:, time_step, :], tf.constant(FLAGS.missing_flag))
+                temp2 = tf.matmul(self.F[time_step - 1], variables_dict["W_imp"]) + variables_dict["bias"]
+                input2 = tf.where(comparison, temp2, self.input_data[:, time_step, :])
+                (cell_output, state) = cell(input2, initial_state=state)
+                self.F.append(cell_output + tf.matmul(self.F[time_step - 1], variables_dict["W_r"]))
+
+        F_out = tf.reshape(tf.concat(self.F, 1), [-1, size])
+        self.prediction = tf.matmul(F_out, variables_dict["W_imp"]) + variables_dict["bias"]
+        targets = tf.reshape(self.targets, [-1, embedding_size])
+        comparison2 = tf.equal(targets, tf.constant(FLAGS.missing_flag))
+        targets = tf.where(comparison2, self.prediction, targets)
+
+        self.cost = tf.reduce_mean(tf.square(targets - self.prediction)) / self.batch_size
+        self.final_state = state
+
+        if not is_training:
+            return
+
+        self._lr = tf.Variable(0.0, trainable=False)
+        self.optimizer = tf.keras.optimizers.Adam(self._lr)
+
+    @tf.function
+    def train_step(self, inputs, targets):
+        with tf.GradientTape() as tape:
+            predictions = self(inputs, training=True)
+            loss = tf.reduce_mean(tf.square(targets - predictions)) / self.batch_size
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        return loss
+
+    def assign_lr(self, lr_value):
+        self.optimizer.learning_rate = lr_value
+
+class TrainConfig:
+    def __init__(self):
+        self.init_scale = 0.1
+        self.num_layers = FLAGS.num_layers
+        self.num_steps = 12
+        self.hidden_size = FLAGS.hidden_size
+        self.batch_size = 4
+        self.embedding_size = FLAGS.embedding_size
+
+class TestConfig:
+    def __init__(self):
+        self.init_scale = 0.1
+        self.num_layers = FLAGS.num_layers
+        self.num_steps = 12
+        self.hidden_size = FLAGS.hidden_size
+        self.batch_size = 4
+        self.embedding_size = FLAGS.embedding_size
+
+def run_epoch(session, model, data, eval_op):
+    header_pre = []
+    pre = []
+    for step, (x, y) in enumerate(reader.ptb_iterator(data, model.batch_size, model.num_steps, FLAGS.embedding_size)):
+        _, prediction = session.run([eval_op, model.prediction], {model.input_data: x, model.targets: y})
+        if step == 0:
+            header_pre.append(prediction[:model.num_steps-1, :])
+        for i in range(model.batch_size):
+            pre.append(prediction[(i+1) * model.num_steps - 1, :])
+    return np.concatenate((np.array(header_pre).reshape(-1, FLAGS.embedding_size), np.array(pre)), axis=0)
+
+def get_config(flag):
+    if flag == "Train":
+        return TrainConfig()
+    elif flag == "Test":
+        return TestConfig()
 
 
-# Función para calcular zt+1
-def calculate_zt_1(W_imprt, rsv_t):
-    # Ajustar la forma de W_imprt si es necesario
-    if len(W_imprt.shape) == 3:
-        W_imprt = tf.squeeze(W_imprt, axis=[0, -1])
+print('Configurando la configuración de entrenamiento')
+config = get_config('Train')
+print("Configuración de entrenamiento establecida.")
+test_config = get_config('Test')
 
-    return tf.matmul(W_imprt, rsv_t)
+print("Leyendo datos...")
+raw_data, columns_name, norlizer = reader.read_raw_data("resultado.txt")
+miss_data = reader.read_missing_data("resultado1.txt", norlizer, 3)
 
-
-# Definir la arquitectura del modelo LSTM
-def build_model(input_shape, output_shape, lstm_units, learning_rate=0.001, l2_penalty=0.001):
-    inputs = Input(shape=input_shape)
-    lstm_layer = LSTM(units=lstm_units, activation='sigmoid', return_sequences=True)(inputs)
-    outputs = Dense(output_shape, kernel_regularizer=regularizers.l2(l2_penalty))(lstm_layer)
-    model = Model(inputs=inputs, outputs=outputs)
-    optimizer = Adam(learning_rate=learning_rate)
-    model.compile(optimizer=optimizer, loss='mse', metrics=['mse'])
-
-    return model
-
-
-data = pd.read_csv('Data/energydata_complete.csv')
-
-X = data[['T1']].values
-y = data[['Appliances']].values
-
-#Normalizar los datos
-scaler = MinMaxScaler()
-X_normalized = scaler.fit_transform(X)
-y_normalized = scaler.fit_transform(y)
-
-
-# Dividir los datos en entrenamiento y prueba
-split_index = int(len(X) * 0.8)
-X_train, X_test = X_normalized[:split_index], X_normalized[split_index:]
-y_train, y_test = y_normalized[:split_index], y_normalized[split_index:]
-
-learning_rate = 0.01
-l2_penalty = 0.01
-batch_size = 256
 epochs = 150
 
-input_shape = (X_train.shape[1], 1)
-inputs = Input(shape=(1, 1))
-lstm_layer = LSTM(units=50, activation='sigmoid', return_sequences=True)(inputs)
-outputs = Dense(1, kernel_regularizer=regularizers.l2(l2_penalty))(lstm_layer)
+print("Configurando las opciones de GPU...")
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+gpu_config = tf.compat.v1.ConfigProto()
+gpu_config.gpu_options.allow_growth = True
+print("Opciones de GPU configuradas.")
 
-output_shape = y_train.shape[1]
-lstm_units = 50
-lambda_target = 0.01  
-W_imprt = tf.Variable(tf.random.normal((input_shape[1], input_shape[1])), trainable=True)
+with tf.compat.v1.Session(config=gpu_config) as session:
+    print("Inicializando variables...")
+    initializer = tf.random_uniform_initializer(-config.init_scale, config.init_scale)
+    with tf.compat.v1.variable_scope("LIMELSTM", reuse=None, initializer=initializer):
+        model = LSTM_RSV(is_training=True, config=config, FLAGS=FLAGS)   	        # train model, is_trainable=True
+    with tf.compat.v1.variable_scope("LIMELSTM", reuse=True, initializer=initializer):
+        test_model = LSTM_RSV(is_training=False, config=test_config, FLAGS=FLAGS)   	        # test model, is_trainable=False
 
-print("Dimensiones de input_shape:", input_shape)
-print("Dimensiones de output_shape:", output_shape)
-print("Dimensiones de W_imprt:", W_imprt.shape)
+    print("Variables inicializadas.")
+    tf.compat.v1.global_variables_initializer().run()
+    model.assign_lr(session, 0.001)
+    new_lr = model._lr
+    for i in range(epochs):
+        _ = run_epoch(session, model, miss_data, model.train_op)
+        if i >= 10 and i % 10 == 0:
+            if new_lr > 0.005:
+                new_lr -= 0.003
+            else:
+                new_lr *= 0.5
+            model.assign_lr(session, new_lr)
+    prediction = run_epoch(session, test_model, miss_data, tf.no_op())
 
-model = build_model(input_shape=(1, 1), output_shape=1, lstm_units=100, learning_rate=learning_rate, l2_penalty=l2_penalty)
+    imputed_data = np.concatenate((miss_data[0, :].reshape(1, -1), np.array(prediction)), axis=0)
+    np.savetxt('imputed_data.txt', imputed_data, delimiter='\t')
 
-optimizer = Adam(learning_rate=learning_rate)
-model.compile(optimizer=optimizer, loss='mse', metrics=['mse'])
-
-model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.2)
-
-# Generar valores faltantes en la serie de tiempo de salida
-missing_indices = np.random.choice(len(y_train), size=int(0.5 * len(y_train)), replace=False)
-y_train_missing = np.copy(y_train)
-y_train_missing = y_train_missing.astype(float)
-y_train_missing[missing_indices] = np.nan 
-
-
-print("Dimensiones de missing_indices:", missing_indices.shape)
-print("Dimensiones de y_train_missing:", y_train_missing.shape)
-
-# Imputar valores faltantes utilizando el RSV
-RSV_train_missing = generate_RSV(model, X_train)
-
-print("Dimensiones de RSV_train_missing:", RSV_train_missing.shape)
-print("Dimensiones de W_imprt:", W_imprt.shape)
-zt_1_train_missing = calculate_zt_1(W_imprt, RSV_train_missing)
-print("Shape of zt_1_train_missing before squeezing:", zt_1_train_missing.shape)
-
-y_train_imputed = np.copy(y_train_missing)
-y_train_imputed[missing_indices] = np.expand_dims(zt_1_train_missing.numpy().flatten(), axis=1)[missing_indices]
-
-#volver a entrenar si es necesario
-#model.fit(X_train, y_train_imputed, epochs=epochs, batch_size=batch_size, validation_split=0.2)
-
-# Calcular la pérdida total después de imputar valores faltantes
-loss_imputed = Ltotal(zt_1_train_missing, X_train, np.ones_like(X_train), model.predict(X_train), y_train_missing, W_imprt, lambda_target)
-print("Total Loss (after imputation):", loss_imputed.numpy())
-
-y_train_imputed_descaled = scaler.inverse_transform(y_train_imputed)
-y_train_descaled = scaler.inverse_transform(y_train)
-
-mse_imputed = mean_squared_error(y_train_descaled, y_train_imputed_descaled)
-print("Mean Squared Error (MSE) entre valores reales y valores imputados:", mse_imputed)
-
-y_pred = model.predict(X_test)
-y_pred = np.squeeze(y_pred, axis=1)
-
-
-# Desescalar las predicciones y los valores reales
-y_pred_descaled = scaler.inverse_transform(y_pred)
-y_test_descaled = scaler.inverse_transform(y_test)
-
-
-# Calcular el MSE entre las predicciones y los valores reales
-mse = mean_squared_error(y_test_descaled, y_pred_descaled)
-print("Mean Squared Error (MSE) en el conjunto de datos de prueba:", mse)
-
-indices = missing_indices
-data['date'] = pd.to_datetime(data['date'])
-# Graficar los valores reales y los valores imputados
-time_index = pd.date_range(start='1/11/2016 17:00', periods=len(y_train_descaled), freq='10min')
-
-
-# Graficar los valores originales y los valores imputados en un gráfico de serie temporal
-plt.figure(figsize=(10, 6))
-plt.plot(time_index, y_train_descaled, label='Valores originales', color='blue')
-plt.plot(time_index, y_train_imputed_descaled, label='Valores imputados', color='red')
-plt.xlabel('Tiempo')
-plt.ylabel('Valor')
-plt.title('Comparación de valores originales y valores imputados')
-plt.legend()
-plt.show()
-
-
-plt.figure(figsize=(10, 6))
-plt.scatter(time_index, y_train_descaled, label='Valores originales', color='blue', marker='o')
-plt.scatter(time_index, y_train_imputed_descaled, label='Valores imputados', color='red', marker='x')
-plt.xlabel('Índice de muestra')
-plt.ylabel('Valor')
-plt.title('Comparación de valores originales y valores imputados')
-plt.legend()
-plt.show()
